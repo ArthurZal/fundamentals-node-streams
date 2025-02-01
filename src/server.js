c5b50ad191e21c62d45c2383885b7ebf232cf7fc
Arthur Zall <zallarthur@gmail.com>
2025-01-31 21:01:46 -0300

    inserção de rotas para teste

--- src/server.js
+++ src/server.js
@@ -4,7 +4,18 @@ const hostName = 'localhost';
 const port = 3000;
 
 const server = createServer((req, res) => {
-    res.end('server');
+    const { method, url } = req;
+
+    if(method === 'GET' && url === '/users') {
+
+        return res.end('Lista de usuários');
+    }
+    if(method === 'POST' && url === '/users') {
+
+        return res.end('Usuário criado com sucesso!');
+    }
+
+
 })
 
 server.listen(port, hostName, () => {
