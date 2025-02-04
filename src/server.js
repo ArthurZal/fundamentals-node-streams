80005b857ac1dfd264633c7148cfc65318901aba
Arthur Zall <zallarthur@gmail.com>
2025-02-03 21:27:11 -0300

    Criação do banco de dados para salvar os usuários dinamicamente

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import { createServer } from 'node:http';
 import { json } from './middlewares/json.js';
+import { Database } from './database.js';
 
 const hostName = 'localhost';
 const port = 3000;
-const users = [];
+const database = new Database();
 
 const server = createServer(async (req, res) => {
     const { method, url } = req;
@@ -11,18 +12,20 @@ const server = createServer(async (req, res) => {
     await json(req, res);
 
     if (method === 'GET' && url === '/users') {
+        const users = database.select('users');
 
-        return res
-            .end(JSON.stringify(users));
+        return res.end(JSON.stringify(users));
     }
     if (method === 'POST' && url === '/users') {
         const { name, email } = req.body;
 
-        users.push({
+        const user = {
             id: 1,
             name,
             email,
-        })
+        }
+
+        database.insert('users', user)
 
         return res.writeHead(201).end();
     }
